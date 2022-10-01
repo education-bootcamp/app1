29b34f65e20c6842a95ab0a080ef5f6fb2368b1a
Sanduni Wijesinghe <73924997+sanduniWijesinghe@users.noreply.github.com>
2022-10-01 09:27:26 +0530

    2

--- src/Demo.java
+++ src/Demo.java
@@ -8,7 +8,28 @@ public class Demo {
     public static void main(String[] args) {
         // savCustomer();
         // getCustomer("C01");
-        // updateCustomer("C01");
+         updateCustomer("C01");
+    }
+
+    private static void updateCustomer(String id) {
+        Configuration configuration= new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Customer.class);
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        Customer tempCustomer = session.get(Customer.class,id);
+        System.out.println("before update : "+tempCustomer);
+
+        tempCustomer.setName("Jayantha");
+        tempCustomer.setAddress("Bandaragama");
+        tempCustomer.setSalary(45000);
+
+        Transaction transaction = session.beginTransaction();
+        session.update(tempCustomer);
+        transaction.commit();
+
+        Customer newCustomer = session.get(Customer.class, id);
+        System.out.println("after update :"+newCustomer);
     }
 
     private static void getCustomer(String id) {

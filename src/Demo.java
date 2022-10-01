3089bd287b812d8dae866f4fe217205bfd69a8ba
Sanduni Wijesinghe <73924997+sanduniWijesinghe@users.noreply.github.com>
2022-10-01 09:38:08 +0530

    3

--- src/Demo.java
+++ src/Demo.java
@@ -2,13 +2,40 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+import java.util.List;
 
 
 public class Demo {
     public static void main(String[] args) {
         // savCustomer();
         // getCustomer("C01");
-         updateCustomer("C01");
+        // updateCustomer("C01");
+        // loadAllCustomers();
+        deleteCustomer();
+    }
+
+    private static void deleteCustomer() {
+        Configuration configuration= new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Customer.class);
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.delete(session.get(Customer.class,"C01"));
+        transaction.commit();
+    }
+
+    private static void loadAllCustomers() {
+        Configuration configuration= new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Customer.class);
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        Query from_customer = session.createQuery("FROM Customer");
+        List list = from_customer.list();
+        System.out.println(list);
     }
 
     private static void updateCustomer(String id) {
